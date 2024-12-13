aa8033501e6eb19b2479d4a83cbb723376065fe9
andi ahmad <andi.saputra@salt.co.id>
2024-12-13 10:34:40 +0700

    add protocol parsing

--- server.cpp
+++ server.cpp
@@ -1,5 +1,8 @@
 #include <arpa/inet.h>
+#include <assert.h>
+#include <cassert>
 #include <cerrno>
+#include <cstdint>
 #include <cstdio>
 #include <errno.h>
 #include <netinet/in.h>
@@ -29,6 +32,74 @@ static void do_something(int connfd) {
 
   char wbuf[] = "world";
   write(connfd, wbuf, strlen(wbuf));
+}
+
+static int32_t read_full(int fd, char *buf, ssize_t n) {
+  while (n > 0) {
+    ssize_t rv = read(fd, buf, n);
+    if (rv <= 0) {
+      return -1; // EOF
+    }
+    assert((ssize_t)rv <= n);
+    n -= (ssize_t)n;
+    buf += rv;
+  }
+  return 0;
+}
+
+static int32_t write_all(int fd, const char *buf, ssize_t n) {
+  while (n > 0) {
+    ssize_t rv = write(fd, buf, n);
+    if (rv <= 0) {
+      return -1; // EOF
+    }
+    assert((ssize_t)rv <= n);
+    n -= (ssize_t)n;
+    buf += rv;
+  }
+  return 0;
+}
+
+const ssize_t k_max_msg = 4096;
+static int32_t one_request(int connfd) {
+  // 4 bytes header
+  char rbuf[4 + k_max_msg + 1];
+  errno = 0;
+  int32_t err = read_full(connfd, rbuf, 4);
+  if (err) {
+    if (errno == 0) {
+      msg("EOF");
+    } else {
+      msg("read() error");
+    }
+    return err;
+  }
+
+  uint32_t len = 0;
+  memcpy(&len, rbuf, 4);
+  if (len > k_max_msg) {
+    msg("to long err");
+    return -1;
+  }
+
+  // request body
+  err = read_full(connfd, &rbuf[4], len);
+  if (err) {
+    msg("read() errxxx");
+    return err;
+  }
+
+  // do something
+  rbuf[4 + len] = '\0';
+  printf("client says: %s\n", &rbuf[4]);
+
+  // reply using the same protocol
+  const char reply[] = "world";
+  char wbuf[4 + sizeof(reply)];
+  len = (uint32_t)strlen(reply);
+  memcpy(wbuf, &len, 4);
+  memcpy(&wbuf[4], reply, len);
+  return write_all(connfd, wbuf, 4 + len);
 }
 
 int main() {
@@ -71,7 +142,14 @@ int main() {
       continue; // error
     }
 
-    do_something(connfd);
+    while (true) {
+      int32_t err = one_request(connfd);
+      if (err) {
+        break;
+      }
+    }
+
+    // do_something(connfd);
     close(connfd);
   }
   return 0;
